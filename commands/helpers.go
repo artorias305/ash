389ddd99383736a378d7d2058ceb189b37ee801c
artorias305 <105161738+artorias305@users.noreply.github.com>
2026-06-20 21:41:30 -0300

    feat: add helper function to check if file is executable

---
+++ commands/helpers.go
@@ -0,0 +1,57 @@
+package commands
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func scanPathForCommand(command string) (string, bool) {
+	pathEnv := os.Getenv("PATH")
+	if pathEnv == "" {
+		fmt.Println("The PATH environment variable is empty.")
+		return "", false
+	}
+
+	paths := filepath.SplitList(pathEnv)
+
+	for _, dir := range paths {
+		// skip empty entries
+		if dir == "" {
+			continue
+		}
+
+		entries, err := os.ReadDir(dir)
+		if err != nil {
+			// fmt.Printf("could not read directory: %v\n\n", err)
+			continue
+		}
+
+		if len(entries) == 0 {
+			continue
+		}
+		for _, entry := range entries {
+			if !entry.IsDir() {
+				fullPath := dir + "/" + entry.Name()
+				executable, err := checkIfCommandIsExecutable(fullPath)
+				if err != nil {
+					log.Fatal(err)
+				}
+				if entry.Name() == command && executable {
+					return fullPath, true
+				}
+			}
+		}
+	}
+	return "", false
+}
+
+func checkIfCommandIsExecutable(command string) (bool, error) {
+	info, err := os.Stat(command)
+	if err != nil {
+		return false, err
+	}
+
+	return info.Mode().Perm()&0111 != 0, nil
+}
--- commands/type.go
+++ commands/type.go
@@ -2,8 +2,6 @@ package commands
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
 
 	"github.com/artorias305/ash/commands/builtin"
 )
@@ -19,39 +17,4 @@ func Type(command string) {
 			fmt.Printf("%s: not found\n", command)
 		}
 	}
-}
-
-func scanPathForCommand(command string) (string, bool) {
-	pathEnv := os.Getenv("PATH")
-	if pathEnv == "" {
-		fmt.Println("The PATH environment variable is empty.")
-		return "", false
-	}
-
-	paths := filepath.SplitList(pathEnv)
-
-	for _, dir := range paths {
-		// skip empty entries
-		if dir == "" {
-			continue
-		}
-
-		entries, err := os.ReadDir(dir)
-		if err != nil {
-			// fmt.Printf("could not read directory: %v\n\n", err)
-			continue
-		}
-
-		if len(entries) == 0 {
-			continue
-		}
-		for _, entry := range entries {
-			if !entry.IsDir() {
-				if entry.Name() == command {
-					return dir + "/" + entry.Name(), true
-				}
-			}
-		}
-	}
-	return "", false
 }

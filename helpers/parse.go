79ca405d6c63ec98831de16501c6456bbdbdc0b1
artorias305 <105161738+artorias305@users.noreply.github.com>
2026-06-21 14:29:50 -0300

    feat: double quote support

--- helpers/parse.go
+++ helpers/parse.go
@@ -6,6 +6,7 @@ func ParseCliInput(input string) []string {
 	var args []string
 	var buf strings.Builder
 	inQuote := false
+	inDoubleQuote := false
 
 	for i := 0; i < len(input); i++ {
 		c := input[i]
@@ -16,10 +17,18 @@ func ParseCliInput(input string) []string {
 			} else {
 				buf.WriteByte(c)
 			}
+		} else if inDoubleQuote {
+			if c == '"' {
+				inDoubleQuote = false
+			} else {
+				buf.WriteByte(c)
+			}
 		} else {
 			switch c {
 			case '\'':
 				inQuote = true
+			case '"':
+				inDoubleQuote = true
 			case ' ', '\t', '\n', '\r':
 				if buf.Len() > 0 {
 					args = append(args, buf.String())
